524083de025e33412ec41480cf71c8d4006d8cfd
YUN SUN <yunsun@YUNs-MacBook-Air.local>
2024-02-26 20:55:21 -0600

    upload

--- CheckAPI.py
+++ CheckAPI.py
@@ -11,3 +11,4 @@ headers = {
 response = requests.request("GET", url, headers=headers, data=payload)
 
 print(response.text)
+# {"code":0,"metalakes":[{"name":"metalake_demo","comment":"comment","audit":{"creator":"anonymous","createTime":"2024-02-26T18:11:13.777Z"}}]}
